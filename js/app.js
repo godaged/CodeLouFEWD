7ecc157f5f6c5cae0e385274c302653b381556b8
godaged <godaged@yahoo.com>
2018-02-03 19:06:24 -0500

    Added dropdown menu with jquery

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// $("#hotels").hover(function() {
+//     $(".hotels").toggleClass("show", "hide");
+// });
+
+$(".museum").mouseover(function() {
+    $(".museum-list").show();
+});
+
+$(".museum").mouseout(function() {
+    $(".museum-list").hide();
+});
