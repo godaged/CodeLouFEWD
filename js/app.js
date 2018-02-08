6b4068fe40b7e8363dc1fe58f29a0e7c3fd5cb6d
godaged <godaged@yahoo.com>
2018-02-07 22:28:14 -0500

    Added menu bar and jqueries

--- js/app.js
+++ js/app.js
@@ -1,11 +1,31 @@
-// $("#hotels").hover(function() {
-//     $(".hotels").toggleClass("show", "hide");
-// });
-
 $(".museum").mouseover(function() {
     $(".museum-list").show();
 });
 
 $(".museum").mouseout(function() {
     $(".museum-list").hide();
+});
+
+
+$(".mobile-bar").click(function() {
+    // if ($(".main-nav").is(":visible"))
+    $(".mobile-barX").show()
+    $(".main-nav").show()
+});
+
+$(".mobile-barX").click(function() {
+    $(".main-nav").hide()
+    $(".mobile-barX").hide()
+});
+
+$(window).resize(function() {
+    if ($(window).width() > 632)
+        $(".main-nav").show()
+    else
+        $(".main-nav").hide()
+});
+
+$(function() {
+    if ($(window).width() > 632)
+        $(".main-nav").show()
 });
