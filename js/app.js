3012415dc663f2db721f17c00e4bf0c12a85716c
godaged <godaged@yahoo.com>
2018-02-17 16:26:12 -0500

    Fixed Mobile Menu, Fixed Jquery, Fixed Slideshow

--- js/app.js
+++ js/app.js
@@ -27,48 +27,52 @@ $(".food").mouseout(function() {
 //Click Hamberger to show mobile menu
 $(".mobile-bar").click(function() {
     $(".mobile-barX").show();
-    $(".main-nav").show();
+    $(".mobile-nav").show();
 });
 
 //Click X to hide mobile menu
 $(".mobile-barX").click(function() {
-    $(".main-nav").hide();
+    $(".mobile-nav").hide();
     $(".mobile-barX").hide();
 });
 
 // Hide & show menu & welcome banner when resizing the webpage
-$(window).resize(function() {
-    if ($(window).width() >= 650) {
-        $(".main-nav").show();
-        $(".welcome").show();
-        $(".mobile-barX").hide();
-    } else {
-        $(".main-nav").hide();
-        $(".mobile-barX").hide();
-        $(".welcome").hide();
-    }
+$(function() {
+    $(window).resize(function() {
+        if ($(window).width() < 650) {
+            $(".mobile-barX").hide();
+            $(".mobile-nav").hide();
+        }
+        if ($(window).width() >= 633) {
+            $(".main-nav").show();
+        }
+    })
 });
 
 
 // Hide mobile menu when scrolling the webpage
-$(window).scroll(function() {
-    if ($(this).scrollTop()) {
-        if ($(window).width() >= 650) {
-            $(".mobile-barX").hide();
-        } else {
-            $(".mobile-barX").hide();
-            $(".main-nav").hide();
+$(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop()) {
+            if ($(window).width() <= 649) {
+                $(".mobile-barX").hide();
+                $(".main-nav").hide();
+            }
+            // else {
+            //     $(".mobile-barX").hide();
+            //     $(".main-nav").show();
+            // }
         }
-    }
+    })
 });
 
 // Hide & show menu & welcome banner when reload & launch the webpage
-$(function() {
-    if ($(window).width() >= 650) {
-        $(".main-nav").show();
-        $(".welcome").show();
-    }
-});
+// $(function() {
+//     if ($(window).width() >= 650) {
+//         $(".main-nav").show();
+//         $(".welcome").show();
+//     }
+// });
 
 
 // Photo slideshow with 5 seconds interval and 3 seconds delay
@@ -80,17 +84,28 @@ $(function() {
 $(function() {
     var images = $('.intro-slide-show-img img'),
         index = 0;
-
+    var i = 0;
     setInterval(looper, 5000);
     looper();
 
     function looper() {
-
-        images.eq(index).fadeIn(2500).delay(3000).fadeOut(0);
-        //using mode is the best but it gives bad lai over images on top
-        //index = (index + 1) % images.length
+        if (i == 0) {
+            images.eq(index).fadeToggle(500).delay(4500);
+            images.eq(index).animate({ width: 'toggle' })
+            images.eq(index).fadeOut(0)
+                //using mode is the best but it gives bad lai over images on top
+                //index = (index + 1) % images.length
+        } else {
+            images.eq(index).fadeToggle(2500).delay(3000).fadeOut(0)
+                //using mode is the best but it gives bad lai over images on top
+                //index = (index + 1) % images.length
+        }
         index += 1;
-        if (index > images.length)
+        if (index > images.length - 1) {
             index = 0;
+            i += 1;
+            if (i > 1)
+                i = 0;
+        }
     }
 })
